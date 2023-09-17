cf50cbb3969777e843c0df64088ea314d5e9984f
Gabriel-ISS <gabriel69iss@gmail.com>
2023-09-16 21:01:21 -0400

    Explicando el caos

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Un reproductor de música muy simplificado
+Un reproductor de música muy simplificado.
+
+Esta lleno de bugs, pero al menos tiene código interesante.
+
+Tiene un sistema de plantillas y es SPA, eso si, solo funciona en local de momento por
+algo relacionado a la forma en como se obtienen las plantillas y la forma en que funciona 
+el servidor de github.
--- scripts/libs/db-service.js
+++ scripts/libs/db-service.js
@@ -1,13 +1,13 @@
 const dbPromise = fetch('/public/db.json')
-    .then(res => res.json())
-    .catch(e => {
-      console.log(e)
-    })
+  .then(res => res.json())
+  .catch(e => {
+    console.log(e)
+  })
 
 export async function getListOfPlaylist() {
   const db = await dbPromise
   // TODO: remover los tracks
-  return db 
+  return db
 }
 
 export async function getPlaylist(index) {
